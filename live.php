3ab81c0905e0819fe74540920cd045de92ddd89e
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-11-29 13:31:30 +0700

    create live page

---
+++ live.php
@@ -0,0 +1,125 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <?php include "templates/header.php"; ?>
+  <?php include "templates/subHeader.php"; ?>
+
+  <!-- MAIN CONTENT -->
+  <div class="home-page__wrapper view-match__wrapper">
+    <div class="container">
+      <div class="home-page__content view-match__content">
+        <div class="view-match__breadcrumb">
+          <a class="view-match__breadcrumb-item" href="#" target="">Home</a>
+          <div class="arrow-right">></div>
+          <a class="view-match__breadcrumb-item" href="#" target="">Football</a>
+          <div class="arrow-right">></div>
+          <a class="view-match__breadcrumb-item" href="#" target="">Houston Astros vs Los Angeles Angels</a>
+        </div>
+
+        <div class="view-match__match-title">
+          <h2 class="match-title__text">Houston Astros vs Los Angeles Angels</h2>
+        </div>
+        <div class="view-match__player">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/zsc4JhnthHc?si=gHhBMJ2Cw2wu4e7J"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        </div>
+        <div class="view-match__bottom">
+          <div class="view-match__list-option-view">
+            <button class="list-option-view__item active">Server 1</button>
+            <button class="list-option-view__item">Server 2</button>
+          </div>
+          <div class="view-match__list-contact">
+            <a class="list-contact__item">
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path
+                  d="M18.3337 10.0003C18.3337 5.40033 14.6003 1.66699 10.0003 1.66699C5.40033 1.66699 1.66699 5.40033 1.66699 10.0003C1.66699 14.0337 4.53366 17.392 8.33366 18.167V12.5003H6.66699V10.0003H8.33366V7.91699C8.33366 6.30866 9.64199 5.00033 11.2503 5.00033H13.3337V7.50033H11.667C11.2087 7.50033 10.8337 7.87533 10.8337 8.33366V10.0003H13.3337V12.5003H10.8337V18.292C15.042 17.8753 18.3337 14.3253 18.3337 10.0003Z"
+                  fill="white" />
+              </svg>
+
+            </a>
+            <a class="list-contact__item">
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path
+                  d="M20 3.83366C19.25 4.16699 18.5 4.41699 17.6667 4.50033C18.5 4.00033 19.1667 3.16699 19.5 2.25033C18.6667 2.75033 17.8333 3.08366 16.9167 3.25033C16.1667 2.41699 15.0833 1.91699 13.9167 1.91699C11.6667 1.91699 9.83333 3.75033 9.83333 6.00033C9.83333 6.33366 9.83333 6.66699 9.91667 6.91699C6.41667 6.75033 3.41667 5.08366 1.41667 2.58366C1 3.25033 0.833333 3.91699 0.833333 4.66699C0.833333 6.08366 1.58333 7.33366 2.66667 8.08366C2 8.08366 1.33333 7.91699 0.833333 7.58366C0.833333 7.58366 0.833333 7.58366 0.833333 7.66699C0.833333 9.66699 2.25 11.3337 4.08333 11.667C3.75 11.7503 3.41667 11.8337 3 11.8337C2.75 11.8337 2.5 11.8337 2.25 11.7503C2.75 13.417 4.25 14.5837 6.08333 14.5837C4.66667 15.667 2.91667 16.3337 1 16.3337C0.666667 16.3337 0.333333 16.3337 0 16.2503C1.83333 17.417 4 18.0837 6.25 18.0837C13.8333 18.0837 17.9167 11.8337 17.9167 6.41699C17.9167 6.25033 17.9167 6.08366 17.9167 5.91699C18.75 5.33366 19.4167 4.58366 20 3.83366Z"
+                  fill="white" />
+              </svg>
+
+            </a>
+            <a class="list-contact__item">
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <g clip-path="url(#clip0_1332_11588)">
+                  <path
+                    d="M5.727 5.2826C5.91653 5.46625 6.17009 5.56894 6.434 5.56894C6.69791 5.56894 6.95147 5.46625 7.141 5.2826C7.32847 5.09507 7.43379 4.84077 7.43379 4.5756C7.43379 4.31044 7.32847 4.05613 7.141 3.8686L5.141 1.8686C4.9524 1.68644 4.6998 1.58565 4.4376 1.58793C4.1754 1.59021 3.92459 1.69538 3.73918 1.88078C3.55377 2.06619 3.44861 2.317 3.44633 2.5792C3.44405 2.8414 3.54484 3.094 3.727 3.2826L5.727 5.2826Z"
+                    fill="white" />
+                  <path
+                    d="M9.5 4.5C9.76522 4.5 10.0196 4.39464 10.2071 4.20711C10.3946 4.01957 10.5 3.76522 10.5 3.5V1C10.5 0.734784 10.3946 0.48043 10.2071 0.292893C10.0196 0.105357 9.76522 0 9.5 0C9.23478 0 8.98043 0.105357 8.79289 0.292893C8.60536 0.48043 8.5 0.734784 8.5 1V3.5C8.5 3.76522 8.60536 4.01957 8.79289 4.20711C8.98043 4.39464 9.23478 4.5 9.5 4.5Z"
+                    fill="white" />
+                  <path
+                    d="M2.90918 9H5.40918C5.6744 9 5.92875 8.89464 6.11629 8.70711C6.30382 8.51957 6.40918 8.26522 6.40918 8C6.40918 7.73478 6.30382 7.48043 6.11629 7.29289C5.92875 7.10536 5.6744 7 5.40918 7H2.90918C2.64396 7 2.38961 7.10536 2.20207 7.29289C2.01454 7.48043 1.90918 7.73478 1.90918 8C1.90918 8.26522 2.01454 8.51957 2.20207 8.70711C2.38961 8.89464 2.64396 9 2.90918 9Z"
+                    fill="white" />
+                  <path
+                    d="M10.7268 17.869L7.32684 21.269C6.84947 21.7212 6.21692 21.9733 5.55934 21.9733C4.90177 21.9733 4.26921 21.7212 3.79184 21.269L2.73084 20.209C2.26454 19.7387 2.0029 19.1033 2.0029 18.441C2.0029 17.7787 2.26454 17.1433 2.73084 16.673L6.13084 13.273C6.22635 13.1808 6.30253 13.0704 6.35494 12.9484C6.40735 12.8264 6.43494 12.6952 6.43609 12.5624C6.43725 12.4296 6.41194 12.2979 6.36166 12.175C6.31138 12.0521 6.23713 11.9405 6.14324 11.8466C6.04934 11.7527 5.93769 11.6785 5.8148 11.6282C5.6919 11.5779 5.56022 11.5526 5.42744 11.5537C5.29466 11.5549 5.16344 11.5825 5.04144 11.6349C4.91943 11.6873 4.80909 11.7635 4.71684 11.859L1.31684 15.259C0.475358 16.1042 0.00292969 17.2483 0.00292969 18.441C0.00292969 19.6337 0.475358 20.7778 1.31684 21.623L2.37784 22.683C3.22288 23.5244 4.36683 23.9968 5.55934 23.9968C6.75185 23.9968 7.8958 23.5244 8.74084 22.683L12.1408 19.283C12.2364 19.1908 12.3125 19.0804 12.3649 18.9584C12.4174 18.8364 12.4449 18.7052 12.4461 18.5724C12.4472 18.4396 12.4219 18.3079 12.3717 18.185C12.3214 18.0621 12.2471 17.9505 12.1532 17.8566C12.0593 17.7627 11.9477 17.6885 11.8248 17.6382C11.7019 17.5879 11.5702 17.5626 11.4374 17.5637C11.3047 17.5649 11.1734 17.5925 11.0514 17.6449C10.9294 17.6973 10.8191 17.7735 10.7268 17.869Z"
+                    fill="white" />
+                  <path
+                    d="M19.377 7H14.877C14.6117 7 14.3574 7.10536 14.1698 7.29289C13.9823 7.48043 13.877 7.73478 13.877 8C13.877 8.26522 13.9823 8.51957 14.1698 8.70711C14.3574 8.89464 14.6117 9 14.877 9H19.377C20.0587 9.01874 20.708 9.29476 21.1946 9.77261C21.6812 10.2505 21.9689 10.8947 22 11.576V13.576C21.9607 14.245 21.6623 14.8723 21.1681 15.3249C20.6739 15.7774 20.0228 16.0196 19.353 16H14.877C14.6117 16 14.3574 16.1054 14.1698 16.2929C13.9823 16.4804 13.877 16.7348 13.877 17C13.877 17.2652 13.9823 17.5196 14.1698 17.7071C14.3574 17.8946 14.6117 18 14.877 18H19.353C20.5553 18.0277 21.7196 17.5776 22.5907 16.7483C23.4617 15.919 23.9685 14.7783 24 13.576V11.576C23.9776 10.3612 23.4819 9.20322 22.6184 8.34851C21.7549 7.49381 20.5919 7.00996 19.377 7Z"
+                    fill="white" />
+                </g>
+                <defs>
+                  <clipPath id="clip0_1332_11588">
+                    <rect width="24" height="24" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+
+              <span class="txt-label">Report</span>
+
+            </a>
+
+          </div>
+        </div>
+
+        <div class="view-match__intro-price-wrapper">
+          <div class="intro-price__main-content">
+            <p class="intro-price__month-label">Plan 3 Months</p>
+            <div class="intro-price__middle-content"></div>
+            <div class="intro-price__get-plan">Get 3 months plan</div>
+
+          </div>
+          <a href="#" target="" title="" class="btn-show-more-plan">
+            <span class="btn-text">See more plan </span>
+            <img src="./images/arrow-right.png" class="icon-arrow" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  <?php include "templates/footer.php"; ?>
+  <script>
+  // Hàm tính toán chiều cao
+  function updateHeight() {
+    var wrapperWidth = $('.view-match__player').width();
+    var newHeight = (wrapperWidth * 9) / 16;
+    $('.view-match__player').css('height', newHeight + 'px');
+  }
+
+  // Sự kiện khi trang được tải
+  $(document).ready(function() {
+    updateHeight(); // Cập nhật chiều cao khi trang được tải
+
+    // Sự kiện resize cửa sổ
+    $(window).resize(function() {
+      updateHeight(); // Cập nhật chiều cao khi cửa sổ được resize
+    });
+  });
+  </script>
+
+</body>
+
+</html>
--- templates/head.php
+++ templates/head.php
@@ -16,6 +16,7 @@
   <link rel="stylesheet" type="text/css" href="css/footer.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="stylesheet" type="text/css" href="css/authen.css">
+				<link rel="stylesheet" type="text/css" href="css/live.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
   <!-- WARNING: Respond.js doesn't work if you view the page via file://-->
